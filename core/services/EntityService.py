c90334f3b080487dfa99cabc75c69381d6c02e7e
Arnaud <arbph@sfr.fr>
2025-12-22 08:21:33 +0100

    Batch1 Step2: Stub EntityService

---
+++ core/services/EntityService.py
@@ -0,0 +1,14 @@
+# /core/services/entity_service.py
+"""
+# dependance
+    
+# classes
+	EntityService
+
+"""
+class EntityService:
+    def __init__(self):
+        pass  # spacy load plus tard
+
+    def extract_entities(self, text_or_doc):
+        return []  # Stub: Retourne empty list pour l'instant
